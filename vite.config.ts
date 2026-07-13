26cdb00b6d6d60b58b2adf29614a545d8b7aca4e
sme12 <sazanovvs@gmail.com>
2026-07-13 19:26:47 +0300

    refactor: remove tanstack devtools to keep things simple

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -8,7 +7,6 @@ import { defineConfig } from "vite";
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
-		devtools(),
 		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		tailwindcss(),
 		tanstackStart(),
